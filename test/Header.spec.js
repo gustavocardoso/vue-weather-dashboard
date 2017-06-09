e0391ddc0a2a3500975d8a2649e12e6b81defc04
Gustavo Cardoso <gustavocardoso@gmail.com>
2017-06-09 13:32:37 -0300

    Adding initial tests

---
+++ test/Header.spec.js
@@ -0,0 +1,25 @@
+import { mount } from 'avoriaz'
+import { expect } from 'chai'
+
+import Header from '../src/components/structure/Header'
+import WdLogo from '../src/components/structure/Logo'
+
+describe('Header component', function () {
+  let wrapper
+  
+  beforeEach(() => {
+    wrapper = mount(Header)
+  });
+
+  it('should has header as root element', () => {
+    expect(wrapper.is('header')).to.equal(true)
+  });
+
+  it('should has a root element with class "footer"', () => {
+    expect(wrapper.hasClass('main-header')).to.equal(true)
+  });
+
+  it('should has wd-logo', () => {
+    expect(wrapper.contains(WdLogo)).to.equal(true)
+  });
+});
