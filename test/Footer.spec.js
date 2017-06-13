6c8b7abeb12b612f3047bba7b1871f252f1dedae
Gustavo Cardoso <gustavocardoso@gmail.com>
2017-06-13 19:20:27 -0300

    Remove semicolon

--- test/Footer.spec.js
+++ test/Footer.spec.js
@@ -6,23 +6,23 @@ import Footer from '../src/components/structure/Footer'
 describe('Footer component', () => {
   let wrapper
 
-  beforeEach(function () {
+  beforeEach(() => {
     wrapper = mount(Footer)
-  });
+  })
 
   it('should has footer as root element', () => {
     expect(wrapper.is('footer')).to.equal(true)
-  });
+  })
 
   it('should has a paragraph with the class "developer"', () => {
     const paragraph = wrapper.find('.developer')
     
     expect(paragraph).to.have.lengthOf(1)
-  });
+  })
 
   it('should has a paragraph with the class "repo"', () => {
     const paragraph = wrapper.find('.repo')
 
     expect(paragraph).to.have.lengthOf(1)
-  });
-});
+  })
+})
