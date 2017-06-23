93a0f1bc42f7ad006e864e0cb261aa9f5d025729
Gustavo Cardoso <gustavocardoso@gmail.com>
2017-06-23 16:29:32 -0300

    Geolocation component tests

--- test/geolocation.spec.js
+++ test/geolocation.spec.js
@@ -2,13 +2,16 @@ import { mount } from 'avoriaz'
 import { expect } from 'chai'
 
 import store from '../src/store/store'
+import mutations from '../src/store/mutations'
 
 import CcMap from '../src/components/geolocation/Map'
+import CcLocationInfo from '../src/components/geolocation/LocationInfo'
 import Geolocation from '../src/components/geolocation/Geolocation'
 
 describe('Geolocation component', () => {
   let wrapper
   let wrapperMap
+  let wrapperLocationInfo
 
   beforeEach(() => {
     wrapperMap = mount(CcMap, {
@@ -24,7 +27,8 @@ describe('Geolocation component', () => {
         showMap: false
       },
       components: {
-        wrapperMap
+        wrapperMap,
+        wrapperLocationInfo
       }
     })
   })
@@ -45,9 +49,35 @@ describe('Geolocation component', () => {
   })
 
   context('Location Info', () => {
-    it('should have a "locationinfo" element', () => {
+    it('should not have a "locationinfo" element if showPosition equals false', () => {
+      wrapper.setData({ showPosition: false })
+      const locInfo = wrapper.find('.locationinfo')[0]
+      expect(wrapper.contains('.locationinfo')).to.equal(false)
+    })
+
+    it('should have a "locationinfo" element if showPosition equals true', () => {
+      wrapper.setData({ showPosition: true })
       const locInfo = wrapper.find('.locationinfo')[0]
       expect(wrapper.contains('.locationinfo')).to.equal(true)
+    })
+
+    it('should display a "locationinfo" element with complete infos when props are passed to it', () => {
+      wrapperLocationInfo = mount(CcLocationInfo, {
+        propsData: {
+          location: 'Perdizes - São Paulo / SP',
+          country: 'Brazil'
+        }
+      })
+
+      wrapper.setData({ showPosition: true })
+
+      //console.log(wrapper.contains('.locationinfo'))
+      //expect(wrapperLocationInfo.contains('.locationinfo')).to.equal(true)
+      const locationInfo = wrapperLocationInfo.find('span')[0].text()
+      const locationInfoCountry = wrapperLocationInfo.find('span')[1].text()
+      
+      expect(locationInfo).to.equal('Perdizes - São Paulo / SP')
+      expect(locationInfoCountry).to.equal('Brazil')
     })
   })
 })
